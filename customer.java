439aef3cbefcd3581b2827956b86973451c68a1e
SheldonRay <47021227+SheldonRay@users.noreply.github.com>
2020-10-13 18:37:37 -0600

    Updated the Customer Class
    All customers are created and ready for manipulation. All are created through the factory method.

--- customer.java
+++ customer.java
@@ -1,7 +1,8 @@
 import java.util.Random;
+
+
 abstract public class customer {
     int rollArray[];
-
     //A total of 1 to 12 casual customers, 1 to 3 business customers, and 1 to 3 catering customers
     public static int RandCasualCustomersNum(){//https://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
         Random rand = new Random();
@@ -15,69 +16,76 @@ abstract public class customer {
         return randomNum;
     }
 
+    public static int RandRollChoice(){//https://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
+        Random rand = new Random();
+        int randomNum = rand.nextInt((5 - 1) + 1) + 1;
+        return randomNum;
+    }
+
     //1 = eggRoll
     //2 = springRoll
     //3 = pastryRoll
     //4 = sausageRoll
     //5 = jellyRoll
-    public static int rollChoice(customer person){//populate array with rolls based on what customer they are
-        return 1;
+    public static void rollChoice(customer person) {//populate array with rolls based on what customer "wants"
+        int rollChoice = 0;
+        Boolean flag = false;
+        if (person.rollArray.length == 15){
+            rollChoice = RandRollChoice();
+            for (int i = 0; i < person.rollArray.length; i++) {//Catering customer selection
+                if ( i>=5 && !flag && i<10){
+                    rollChoice = RandRollChoice();
+                    flag = true;
+                }
+                if (i>=10 && flag ){
+                    rollChoice = RandRollChoice();
+                    flag = false;
+                }
+                person.rollArray[i] = rollChoice;
+            }
+        } else {
+            for (int i = 0; i < person.rollArray.length; i++) {//Casual Customer selection
+                rollChoice = RandRollChoice();
+                person.rollArray[i] = rollChoice;
+            }
+        }
     }
+    //Should be a function in Resturant that is able to change the roll array depending on if they dont have enough stock
 
-    public static void main(String args[]){
-        customer casual = new casualCustomer();
-        customer buis = new businessCustomer();
-        customer cater = new cateringCustomer();
-        System.out.println(casual.rollArray.length);
-        System.out.println(buis.rollArray.length);
-        System.out.println(cater.rollArray.length);
-    }
-
-    public void createCustomers(){//Make a for loop and create all of them per day
-        int numCasualCustomers = customer.RandCasualCustomersNum();
-        int numBusinessCustomers = customer.RandBusinessAndCateringCustomersNum();
-        int numCateringCustomers = customer.RandBusinessAndCateringCustomersNum();
+    public static void main(String args[]){//Main for testing customer stuff
+        customerFactory factory = new customerFactory();//AN ABSTRACT CLASS THAT HAS THE PRODUCTS OF THE FACTORY
+        customer casual = factory.createCustomer("casual");
+        customer cater = factory.createCustomer("catering");
+        customer buis = factory.createCustomer("business");
+        for (int i = 0; i < 3 ; i++) {
+            System.out.println(casual.rollArray[i]);
+        }
+        return;
     }
 }
 
-
 //Start Customers
-class casualCustomer extends customer{//make some thing here, maybe a buy function based on the rand number of rolls they wanna buy
-    int totalNumCasualCustomers = 0;
-    int dailyNumCasualCustomers = 0;
+class casualCustomer extends customer{//CONCRETE PRODUCTS
     casualCustomer(){//upon creation each casual customer given an array of size 3
         rollArray = new int[3];
-        for (int i = 0; i < 3; i++) {
-            this.rollArray[i] = -1;
-        }
+        rollChoice(this);
     }
-
 }
 
 class businessCustomer extends customer{
-    int totalBusinessCustomers = 0;
-    int dailyBusinessCustomers = 0;
     businessCustomer(){
         rollArray = new int[10];
-        for (int i = 0; i < 10 ; i++) {
-            this.rollArray[i] = -1;
-        }
+        rollChoice(this);
     }
 }
 
 class cateringCustomer extends customer {
-    int totalCateringCustomers = 0;
-    int dailyCateringCustomers = 0;
     cateringCustomer() {
         rollArray = new int[15];
-        for (int i = 0; i < 15; i++) {
-            this.rollArray[i] = -1;
-        }
+        rollChoice(this);
     }
 }
-//End customers
+//End customers AND CONCRETE PRODUCTS
 
 
-// Casual customers will buy 1 to 3 rolls (Rand)
-// Business customers will buy 10 rolls, two of each type.
 // Catering customers will buy 5 rolls of 3 different types, 15 total (Rand for type)
