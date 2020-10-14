0fac396cf895ff6027cb8549466f492a3822d004
jacobsr1146 <54606162+jacobsr1146@users.noreply.github.com>
2020-10-13 20:17:31 -0600

    Add files via upload

---
+++ UnitTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+
+import jdk.internal.jshell.tool.resources.version;
+
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void checkDaySalesAreReset() {
+        
+
+    }
+
+    @Test
+    public void checkInventoryIsRefilled() {
+        
+
+    }
+
+    @Test
+    public void checkStoreClosedEarly() {
+        
+
+    }
+
+    @Test
+    public void checkCustomerOrderIsFullfilled() {
+        
+
+    }
+    @Test
+    public void checkCustomerOutageOrderIsFullfilled() {
+        
+
+    }
+    @Test
+    public void checkDailySalesAreAddedToTotal() {
+        
+
+    }
+
+    @Test
+    public void checkExtrasAreAdded() {
+        
+
+    }
+
+    @Test
+    public void checkCasualCustomerGetsCorrectOrder() {
+        
+
+    }
+
+    @Test
+    public void checkBusinessCustomerGetsCorrectOrder() {
+        
+
+    }
+
+    @Test
+    public void checkCateringCustomerGetsCorrectOrder() {
+        
+
+    }
+}
